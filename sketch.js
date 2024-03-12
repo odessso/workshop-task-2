b9f00a0b65e0c8adca135ca03befc487dcd7c15b
odessso <162661640+odessso@users.noreply.github.com>
2024-03-12 15:54:49 +1300

    Added project files (possibly needs code edit)

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let counter = 0;
+
+let countInterval = setInterval(makeRandomSquare, 1);
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+}
+
+function mousePressed(){
+  setInterval(makeRandomSquare, 10);
+}
+
+function makeRandomSquare(){
+  noStroke();
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  rect(random(0, width), random(0, height), random(1, 3), random(1, 8000));
+  counter++;
+  if (counter > 1000){
+    clearInterval(countInterval); 
+    counter = 0;
+  }
+}
+
+function mousePressed(){
+  countInterval = setInterval(makeRandomSquare, 1);
+}
+
+// function makeBoringSquare(){
+//   stroke(255);
+//   noFill();
+//   rect(random(0, width), random(0, height), 10, 10);
+// }
+
+function draw() {
+}
